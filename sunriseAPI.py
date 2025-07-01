aa3c48da582a4af82a354aa38bbb67119a81d5c7
Thaninux <guerninenina@gmail.com>
2025-07-01 10:28:50 +0200

    get sunrise/sunset from API

---
+++ sunriseAPI.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+
+# Coordinates
+lat = 44.138062
+lng = 4.810250
+
+# API endpoint
+url = f"https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}&formatted=0"
+
+
+response = requests.get(url)
+
+
+folder_path = "data_like/lampadaires"
+file_path = os.path.join(folder_path, "sunrise_sunset.json")
+
+
+os.makedirs(folder_path, exist_ok=True)
+
+if response.status_code == 200:
+    data = response.json()
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+    print(f"Data saved to '{file_path}'")
+else:
+    print("Failed to fetch data:", response.status_code)
